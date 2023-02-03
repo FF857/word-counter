843ca461c1ffdfa853dd6c5cb8a3f2adfeb03d12
FF857 <eufelipe857@gmail.com>
2023-02-03 12:24:04 -0300

    add scrip

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function calcular() {
+  var text = document.getElementById("text-area").value;
+  var calculo = text.split(" ").length;
 
+  document.getElementById("resultado").innerHTML =
+    "{ Você tem: " + calculo + " palavras escritas }";
+}
